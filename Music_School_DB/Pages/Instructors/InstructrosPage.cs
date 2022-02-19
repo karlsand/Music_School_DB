39d4a380894c5e753bc097ca9886c6717e20bb55
karlksander <karl.k.sander@gmail.com>
2022-02-19 15:49:23 +0200

    õppevideo 2 lõpetamine

--- Music_School_DB/Pages/Instructors/InstructrosPage.cs
+++ Music_School_DB/Pages/Instructors/InstructrosPage.cs
@@ -1,26 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Music_School_DB.Domain.Party;
+using Music_School_DB.Facade.Party;
 
 namespace Music_School_DB.Pages.Instructors
 {
     public class InstructrosPage : PageModel
     {
-        private readonly Music_School_DB.Data.Music_School_DBContext _context;
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
 
-        public CreateModel(Music_School_DB.Data.Music_School_DBContext context)
-        {
-            _context = context;
-        }
-
-        public IActionResult OnGet()
-        {
-            return Page();
-        }
+        private readonly Data.Music_School_DBContext context;
 
         [BindProperty]
         public InstructorView Instructor { get; set; }
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        public IList<InstructorView> Instructors { get; set; }
+
+        public InstructrosPage(Data.Music_School_DBContext c)
+        {
+            context = c;
+        }
+
+        public IActionResult OnGetCreate()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostCreateAsync()
         {
             if (!ModelState.IsValid)
             {
@@ -28,10 +37,111 @@ namespace Music_School_DB.Pages.Instructors
             }
             var d = new InstructorViewFactory().Create(Instructor).Data;
 
-            _context.Instructors.Add(d);
-            await _context.SaveChangesAsync();
+            context.Instructors.Add(d);
+            await context.SaveChangesAsync();
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("./Index", "Index");
+        }
+
+        public async Task<IActionResult> OnGetDetailsAsync(string id)
+        {
+            Instructor = await getInstructor(id);
+
+            return Instructor == null ? NotFound() : Page();
+        }
+
+        private async Task<InstructorView> getInstructor(string id)
+        {
+            if (id == null)
+            {
+                return null;
+            }
+
+            var d = await context.Instructors.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (d == null)
+            {
+                return null;
+            }
+
+            return new InstructorViewFactory().Create(new Instructor(d));
+        }
+
+        public async Task<IActionResult> OnGetDeleteAsync(string id)
+        {
+            Instructor = await getInstructor(id);
+
+            return Instructor == null ? NotFound() : Page();
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var d = await context.Instructors.FindAsync(id);
+
+            if (d != null)
+            {
+                context.Instructors.Remove(d);
+                await context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index", "Index");
+        }
+
+        public async Task<IActionResult> OnGetEditAsync(string id)
+        {
+            Instructor = await getInstructor(id);
+
+            return Instructor == null ? NotFound() : Page();
+        }
+
+        public async Task<IActionResult> OnPostEditAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            var d = new InstructorViewFactory().Create(Instructor).Data;
+
+            context.Attach(d).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!instructorExists(Instructor.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index", "Index");
+        }
+
+        private bool instructorExists(string id)
+        {
+            return context.Instructors.Any(e => e.ID == id);
+        }
+
+        public async Task OnGetIndexAsync()
+        {
+            var list = await context.Instructors.ToListAsync();
+            Instructors = new List<InstructorView>();
+            foreach (var d in list)
+            {
+                var v = new InstructorViewFactory().Create(new Instructor(d));
+                Instructors.Add(v);
+            }
         }
     }
 }
