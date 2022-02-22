14949e77e48482ee4b3c7a3f6b38f84276ee080b
karlksander <karl.k.sander@gmail.com>
2022-02-23 00:10:39 +0200

    data test class

---
+++ Tests/Data/Party/InstructorDataTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Music_School_DB.Tests.Data.Party
+{
+    [TestClass]
+    public class InstructorDataTests : AssertTests
+    {
+        [TestMethod] public void IDTest() => inconclusive();
+        [TestMethod] public void InstrumentIDTest() => inconclusive();
+        [TestMethod] public void FirstNameTest() => inconclusive();
+        [TestMethod] public void LastNameTest() => inconclusive();
+        [TestMethod] public void EmailTest() => inconclusive();
+        [TestMethod] public void PhoneNrTest() => inconclusive();
+    }
+
+    public class AssertTests
+    {
+        public void inconclusive() => Assert.Inconclusive();
+    }
+
+    public class BaseTests : AssertTests
+    {
+        public void inconclusive() => Assert.Inconclusive();
+    }
+}
