2aef5d7ffbbfe31cee2d06aa225e46ac9c41f731
karlksander <karl.k.sander@gmail.com>
2022-03-09 02:41:47 +0200

    mallid

---
+++ Pages/Extensions/MyViewerForHtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Linq.Expressions;
+
+namespace Music_School_DB.Pages.Extensions
+{
+    public static class MyViewerForHtml
+    {
+        public static IHtmlContent MyViewerFor<TModel, TResult>(this IHtmlHelper<TModel> html, Expression<Func<TModel, TResult>> expression)
+        {
+            var s = htmlStrings(html, expression);
+            return new HtmlContentBuilder(s);
+        }
+
+        private static List<object> htmlStrings<TModel, TResult>(IHtmlHelper<TModel> h, Expression<Func<TModel, TResult>> e)
+        {
+            var l = new List<object>();
+            l.Add(new HtmlString("<dl class=\"row\">"));
+            l.Add(new HtmlString("<dt class=\"col-sm-2\">"));
+            l.Add(h.DisplayNameFor(e));
+            l.Add(new HtmlString("</dt>"));
+            l.Add(new HtmlString("<dd class=\"col-sm-10\">"));
+            l.Add(h.DisplayFor(e));
+            l.Add(new HtmlString("</dd>"));
+            l.Add(new HtmlString("</dl>"));
+            return l;
+        }
+    }
+}
